b3efd06311094dbfcf892f36aa5b77dff7aa5968
Yeohuiju <yeohuiju03@gmail.com>
2025-04-11 18:05:24 +0900

    [REFACTOR] 도메인형으로 변경

---
+++ spring/demo/src/main/java/com/example/demo/event/enums/EventStatus.java
@@ -0,0 +1,10 @@
+package com.example.demo.event.enums;
+
+public enum EventStatus {
+    APPLY_NOT_OPENED,       // 응모 전
+    APPLY_OPEN,             // 응모 중
+    APPLY_CLOSED,           // 응모 마감
+    TICKETED,               // 예매 완료
+    IN_PROGRESS,            // 공연 중
+    ENDED,                  // 공연 종료
+}
