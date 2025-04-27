577d02733bc96f41a6ac78b9375e17289de4fef4
Yeohuiju <yeohuiju03@gmail.com>
2025-04-28 04:02:20 +0900

    [FEAT] 개최자 홈 수정

--- spring/demo/src/main/java/com/example/demo/domain/main/controller/OrganizerHomeController.java
+++ spring/demo/src/main/java/com/example/demo/domain/main/controller/OrganizerHomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.main.controller;
 
+import com.example.demo.domain.main.dto.EventCardListDTO;
 import com.example.demo.domain.main.dto.OrganizerHomeResponseDTO;
 import com.example.demo.domain.main.service.OrganizerHomeService;
 import com.example.demo.global.response.ApiResponse;
@@ -21,6 +22,24 @@ public class OrganizerHomeController {
     @Operation(summary = "개최자 - 홈 화면 조회")
     @GetMapping("")
     public ApiResponse<OrganizerHomeResponseDTO> getOrganizerHome() {
-        return ApiResponse.onSuccess(_OK, organizerHomeService.getOrganizerHome());
+        return ApiResponse.onSuccess(_OK, organizerHomeService.getHomeForOrganizer());
+    }
+
+    @Operation(summary = "개최자 - 오늘 공연 조회")
+    @GetMapping("/today")
+    public ApiResponse<EventCardListDTO> getTodayEventsOrganizer() {
+        return ApiResponse.onSuccess(_OK, organizerHomeService.getTodayEventsForOrganizer());
+    }
+
+    @Operation(summary = "개최자 - 최근 응모 마감 공연 조회")
+    @GetMapping("/closed")
+    public ApiResponse<EventCardListDTO> getClosedEventsOrganizer() {
+        return ApiResponse.onSuccess(_OK, organizerHomeService.getClosedEventsForOrganizer());
+    }
+
+    @Operation(summary = "개최자 - 전체 공연 조회")
+    @GetMapping("/all")
+    public ApiResponse<EventCardListDTO> getAllEventsOrganizer() {
+        return ApiResponse.onSuccess(_OK, organizerHomeService.getAllEventsForOrganizer());
     }
 }
