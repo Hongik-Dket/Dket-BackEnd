bcac331ec29aeb5b50effeb92141d6bd5f4da4c9
이지우 <insa658723@gmail.com>
2025-06-02 02:34:33 +0900

    feat: TicketRepository 생성

---
+++ src/main/java/com/example/demo/domain/ticket/repository/TicketRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain.ticket.repository;
+
+import com.example.demo.domain.event.entity.Event;
+import com.example.demo.domain.ticket.entity.Ticket;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("""
+        SELECT DISTINCT t.session.event
+        FROM Ticket t
+        WHERE t.user.id = :buyerId
+        ORDER BY 
+            CASE WHEN t.session.event.eventStatus = 'ENDED' THEN 1 ELSE 0 END ASC,
+            t.session.event.startDate ASC,
+            t.session.event.endDate DESC
+    """)
+    List<Event> findPurchasedEventsByBuyer(@Param("buyerId") Long buyerId, Pageable pageable);
+}
