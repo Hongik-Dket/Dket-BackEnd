fdfa2f66780545bb8ee68a1c2c82760d0729e0b5
이지우 <insa658723@gmail.com>
2025-06-01 16:50:48 +0900

    feat: BuyerHomeService 생성

---
+++ src/main/java/com/example/demo/domain/main/service/BuyerHomeService.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.main.service;
+
+import com.example.demo.domain.main.dto.BuyerHomeResponseDTO;
+import com.example.demo.domain.main.dto.EventCardListDTO;
+
+public interface BuyerHomeService {
+
+    BuyerHomeResponseDTO getHomeForBuyer();
+
+    EventCardListDTO getPopularEvents();
+
+    EventCardListDTO getAppliedEvents();
+
+    EventCardListDTO getPurchasedEvents();
+}
