e41cce40d990e7ea9b10043cc91cdd9703b52716
heejuyeo <yeohuiju03@gmail.com>
2025-10-24 01:55:41 +0900

    [FIX] replace OpenSea URL with Etherscan

--- src/main/java/com/example/demo/domain/ticket/service/TicketServiceImpl.java
+++ src/main/java/com/example/demo/domain/ticket/service/TicketServiceImpl.java
@@ -166,7 +166,7 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public String getNftUrl(Ticket ticket) {
-        return Constants.OPENSEA_BASE_URL + contractAddress + "/%s".formatted(ticket.getTokenId());
+        return Constants.ETHERSCAN_NFT_BASE_URL + contractAddress + "/%s".formatted(ticket.getTokenId());
     }
 
     private void validateOrganizer(Ticket ticket, User user) {
--- src/main/java/com/example/demo/global/base/Constants.java
+++ src/main/java/com/example/demo/global/base/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     // 공연 시작 전 결제 가능 기한(시간 단위): 공연 시작 2시간 전까지 결제 가능
     public static final int PAYMENT_AVAILABLE_BEFORE_CONCERT_START = 2;
 
-    // OpenSea base url
-    public static final String OPENSEA_BASE_URL = "https://testnets.opensea.io/assets/sepolia/";
+    // Etherscan NFT base url
+    public static final String ETHERSCAN_NFT_BASE_URL = "https://sepolia.etherscan.io/nft/";
 
     // 리세일 티켓 예약 만료 시간(분 단위)
     public static final int RESALE_RESERVATION_EXPIRATION_MINUTES = 5;
