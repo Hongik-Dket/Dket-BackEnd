686991c0374f3c9a02575d005c9cc379daf43cdd
heejuyeo <yeohuiju03@gmail.com>
2025-11-17 22:20:44 +0900

    [CHORE] 온체인 트랜잭션 로그 추가

--- src/main/java/com/example/demo/global/infra/blockchain/service/DketResaleService.java
+++ src/main/java/com/example/demo/global/infra/blockchain/service/DketResaleService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface DketResaleService {
 
-    String listResaleOnChain(Resale resale);
+    void listResaleOnChain(Resale resale);
 
     void cancelResaleBatch(List<Long> resaleIds);
 
